8c89133e88a6d6f5f17575f721bb209061f700ea
lalaaichac <lalaaichac@gmail.com>
2023-10-23 23:08:53 -0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,17 @@
+var randomNumber1 = Math.floor(Math.random()*6) + 1;
+//document.querySelector(".img1").setAttribute("src","./images/dice"+randomNumber1+".png");
+changeImage(".img1", randomNumber1);
+var randomNumber2 = Math.floor(Math.random()*6) + 1;
+//document.querySelector(".img2").setAttribute("src","./images/dice"+randomNumber2+".png");
+changeImage(".img2", randomNumber2);
+
+function changeImage(src, name){
+    document.querySelector(src).setAttribute("src","./images/dice"+ name +".png");
+}
+if (randomNumber1 > randomNumber2){
+    document.querySelector("h1").innerHTML = "Player 1 Wins 🚩";
+} else if (randomNumber2 > randomNumber1){
+    document.querySelector("h1").innerHTML = "Player 2 Wins 🚩";
+} else{
+    document.querySelector("h1").innerHTML = "Draw";
+}
